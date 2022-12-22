a8decd1fe4a293bc8135673f47fa08f691324672
tijil <tijilmalhotra@gmail.com>
2022-12-22 20:48:06 +0530

    login page front-end made

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,13 @@
+
+import React from "react";
 import './App.css';
+import LoginPage from "./pages/Login/loginpage.js"
+// import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
-      Hello
+    <LoginPage/>
     </div>
   );
 }
