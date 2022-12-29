1e4d45ad5179d4945097aad02a80d5f395b726ed
Aitchessbee <bediharsiddak@gmail.com>
2022-12-29 15:10:17 +0530

    profile page done

--- frontend/src/pages/Profile/Components/ccsbulb.js
+++ frontend/src/pages/Profile/Components/ccsbulb.js
@@ -1,18 +1,62 @@
-import React, {useContext} from "react";
+import React, {useContext, useEffect, useState} from "react";
 import styles from "../Style/ccsbulb.module.css";
 import Navbar from "./navbar.js"
 import AuthContext from "../../../store/auth-context";
 
 import ccsbulb from "../Assets/CCS_Bulb.png"
 
-
 import Zoom from 'react-reveal/Zoom';
 import Reveal from 'react-reveal/Reveal';
 import Fade from 'react-reveal/Fade';
 import Flip from 'react-reveal/Flip';
+import axios from "axios";
+
+import { api_url } from "../../../config";
+const user_url = api_url + "auth/user/"
+const orders_url = api_url + "order/all/"
 
 function CcsBulb() {
   const authCtx = useContext(AuthContext)
+
+  const [userDetails, setUserDetails] = useState({});
+  const [previousOrders, setPreviousOrders] = useState([]);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token")
+
+    const config = {
+        headers: {
+            "Authorization": `Token ${token}`
+        }
+    }
+
+    const fetchData = async () => {
+      const res = await axios.get(user_url, config)
+      console.log(res.data)
+      setUserDetails(res.data);
+    }
+
+    fetchData()
+
+  }, [])
+
+  useEffect(() => {
+    const token = localStorage.getItem("token")
+
+    const config = {
+        headers: {
+            "Authorization": `Token ${token}`
+        }
+    }
+
+    const fetchData = async () => {
+      const res = await axios.get(orders_url, config)
+      setPreviousOrders(res.data);
+    }
+
+    fetchData()
+
+  }, [])
 
   return (
     <div className={styles.container}>
@@ -28,7 +72,7 @@ function CcsBulb() {
           }}
         />
         <div className={styles.subcontainer}>
-          User: Name Surname
+          User: {userDetails.name}
         </div>
         <div className={styles.subcontainer}>
           Previous Orders :-
@@ -36,20 +80,30 @@ function CcsBulb() {
 
         <div class={styles.parent}>
 
-          <div className={styles.child}>
-            <div className={styles.order}>
-              if no orders show null
+          { previousOrders.length == 0 &&
+            <div className={styles.child}>
+              <div className={styles.order}>
+                You have no orders you poor piece of shit
+              </div>
             </div>
-            <div className={styles.order}>
-              orderdetails
+          }
+
+          { previousOrders &&
+            <div className={styles.child}>
+              {previousOrders.map(item => {
+                return (
+                  <div className={styles.order}>
+                    {item.order_items.map(product => {
+                      return (
+                        <div>{product.product.name} {product.printing_name} {product.size} {product.product.price}</div>
+                      )
+                    })}
+                    <div>{item.amount}</div>
+                  </div>
+                )
+              })}
             </div>
-            <div className={styles.order}>
-              orderdetails
-            </div>
-            <div className={styles.order}>
-              orderdetails
-            </div>
-          </div>
+          }
 
         </div>
 
