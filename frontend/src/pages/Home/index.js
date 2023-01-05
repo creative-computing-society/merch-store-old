dcfc7be034025d49faad1a11fb79134db14912de
Aitchessbee <bediharsiddak@gmail.com>
2023-01-05 23:09:54 +0530

    added overflow hidden on home

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -5,7 +5,7 @@ import Hero from "./Components/Hero";
 
 function Home() {
   return (
-    <div style={{backgroundColor: "#eeeeee"}}>
+    <div style={{backgroundColor: "#eeeeee", overflowX: "hidden"}}>
       <Hero/>
       <Products />
     </div>
