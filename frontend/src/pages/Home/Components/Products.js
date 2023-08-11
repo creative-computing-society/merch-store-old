730c94a4ffc8893d0c22ac458993d730ae7befa5
Aitchessbee <bediharsiddak@gmail.com>
2023-08-11 23:53:50 +0530

    fixed card size

--- frontend/src/pages/Home/Components/Products.js
+++ frontend/src/pages/Home/Components/Products.js
@@ -1,67 +1,67 @@
-import React, { useState, useEffect, useContext } from 'react'
-import axios from "axios"
-import { Link } from 'react-router-dom';
-import styles from "../Style/products.module.css"
-import AuthContext from '../../../store/auth-context';
+import React, { useState, useEffect, useContext } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+import styles from "../Style/products.module.css";
+import AuthContext from "../../../store/auth-context";
 
-import { api_url } from '../../../config';
-const url = api_url + "product/all/"
+import { api_url } from "../../../config";
+const url = api_url + "product/all/";
 
 function Products() {
-  const authCtx = useContext(AuthContext)
-  
-  const [products, setProducts] = useState();
-  
+    const authCtx = useContext(AuthContext);
 
-  useEffect(() => {
+    const [products, setProducts] = useState();
 
-    
-    const fetchData = async () => {
-      // const res = await axios.get(url)
-      let res;
-      const token = localStorage.getItem("token")
-      if(token) {
-        // const token = localStorage.getItem("token")
+    useEffect(() => {
+        const fetchData = async () => {
+            // const res = await axios.get(url)
+            let res;
+            const token = localStorage.getItem("token");
+            if (token) {
+                // const token = localStorage.getItem("token")
 
-        const config = {
-          headers: {
-            "Authorization": `Token ${token}`
-          }
-        }
+                const config = {
+                    headers: {
+                        Authorization: `Token ${token}`,
+                    },
+                };
 
-        res = await axios.get(url, config)
-      } else {
-        res = await axios.get(url)
-      }
-      
-      const data = res.data
-      // console.log(data)
-      setProducts(data.map(product => {
+                res = await axios.get(url, config);
+            } else {
+                res = await axios.get(url);
+            }
 
-        return (
-          <div className={styles.product}>
-            <Link to={`/product/${product.id}`}>
-                <div><img src={product.image1} alt={product.name} className={styles.carouselImage} /></div>
-            </Link>
-            <div className={styles.productName}>
-            <div ><b>{product.name}</b></div>
-            <div >₹{product.price}</div>
-            </div>
-          </div>
-        )
+            const data = res.data;
+            // console.log(data)
+            setProducts(
+                data.map((product) => {
+                    return (
+                        <div className={styles.product}>
+                            <Link to={`/product/${product.id}`}>
+                                <div>
+                                    <img
+                                        src={product.image1}
+                                        alt={product.name}
+                                        className={styles.carouselImage}
+                                    />
+                                </div>
+                            </Link>
+                            <div className={styles.productName}>
+                                <div>
+                                    <b>{product.name}</b>
+                                </div>
+                                <div>₹{product.price}</div>
+                            </div>
+                        </div>
+                    );
+                })
+            );
+        };
 
-      }))
-    }
+        fetchData();
+    }, []);
 
-    fetchData()
-
-  }, [])
-
-  return (
-    <div class={styles.products}>
-      {products}
-    </div>
-  )
+    return <div class={styles.products}>{products}</div>;
 }
 
-export default Products
+export default Products;
